a5a0e07ad7989c17f3459316f90908b68dfbe15d
Krishnan Vellore <vskrishnan@tamu.edu>
2023-12-18 12:22:57 -0600

    changed all jpg to webp

--- src/app/_components/home/Carousel.tsx
+++ src/app/_components/home/Carousel.tsx
@@ -4,37 +4,37 @@ import Image from "next/image";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 const orgImages = [
-  "/images/photos/20230119_180722.jpg",
-  "/images/photos/20230119_181122.jpg",
-  "/images/photos/20230119_181127.jpg",
-  "/images/photos/20230119_181204.jpg",
-  "/images/photos/20230119_181219.jpg",
-  "/images/photos/20230119_181249.jpg",
-  "/images/photos/20230119_181722.jpg",
-  "/images/photos/70354378340__2697ED96-1234-438F-9107-3D0A08A82467.jpg",
-  "/images/photos/IMG_5627.jpg",
-  "/images/photos/IMG_5628.jpg",
-  "/images/photos/IMG_5635.jpg",
-  "/images/photos/IMG_5675.jpg",
-  "/images/photos/IMG_5692.jpg",
-  "/images/photos/IMG_5695.jpg",
-  "/images/photos/IMG_5711.jpg",
-  "/images/photos/IMG_5722.jpg",
-  "/images/photos/IMG_5723.jpg",
-  "/images/photos/IMG_5736.jpg",
-  "/images/photos/IMG_5741.jpg",
-  "/images/photos/IMG_5745.jpg",
-  "/images/photos/IMG_5754.jpg",
-  "/images/photos/IMG_5786.jpg",
-  "/images/photos/IMG_5838.jpg",
-  "/images/photos/IMG_5856.jpg",
-  "/images/photos/IMG_5861.jpg",
-  "/images/photos/IMG_5882.jpg",
-  "/images/photos/IMG_5996.jpg",
-  "/images/photos/IMG_6116.JPG",
-  "/images/photos/IMG_6117.JPG",
-  "/images/photos/IMG_6119.JPG",
-  "/images/photos/IMG_6121.jpg",
+  "/images/photos/20230119_180722.webp",
+  "/images/photos/20230119_181122.webp",
+  "/images/photos/20230119_181127.webp",
+  "/images/photos/20230119_181204.webp",
+  "/images/photos/20230119_181219.webp",
+  "/images/photos/20230119_181249.webp",
+  "/images/photos/20230119_181722.webp",
+  "/images/photos/70354378340__2697ED96-1234-438F-9107-3D0A08A82467.webp",
+  "/images/photos/IMG_5627.webp",
+  "/images/photos/IMG_5628.webp",
+  "/images/photos/IMG_5635.webp",
+  "/images/photos/IMG_5675.webp",
+  "/images/photos/IMG_5692.webp",
+  "/images/photos/IMG_5695.webp",
+  "/images/photos/IMG_5711.webp",
+  "/images/photos/IMG_5722.webp",
+  "/images/photos/IMG_5723.webp",
+  "/images/photos/IMG_5736.webp",
+  "/images/photos/IMG_5741.webp",
+  "/images/photos/IMG_5745.webp",
+  "/images/photos/IMG_5754.webp",
+  "/images/photos/IMG_5786.webp",
+  "/images/photos/IMG_5838.webp",
+  "/images/photos/IMG_5856.webp",
+  "/images/photos/IMG_5861.webp",
+  "/images/photos/IMG_5882.webp",
+  "/images/photos/IMG_5996.webp",
+  "/images/photos/IMG_6116.webp",
+  "/images/photos/IMG_6117.webp",
+  "/images/photos/IMG_6119.webp",
+  "/images/photos/IMG_6121.webp",
 ]
 const delay = 3000;
 
@@ -80,7 +80,7 @@ export default function Carousel() {
       <div className="w-screen whitespace-nowrap transition-all ease-in-out duration-1000" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
         {orgImages.map((img, idx) => (
           <div key={idx} className={`inline-block h-full w-full`}>
-            <Image src={img} alt="client logo" className="mx-auto h-auto w-9/12" width={0} height={0} />
+            <Image src={img} alt="client logo" className="mx-auto h-auto w-9/12" width={0} height={0}/>
           </div>
         ))}
       </div>
