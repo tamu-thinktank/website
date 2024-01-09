6e139a2e2e580f1563457d0212afd19011cc7636
Krishnan Vellore <vskrishnan@tamu.edu>
2024-01-09 15:07:15 -0600

    updated intro card

--- src/app/apply/_sections/intro.tsx
+++ src/app/apply/_sections/intro.tsx
@@ -25,36 +25,6 @@ export default function FormIntroTab() {
           requirements, and other relevant factors.
           <br /> <br />
         </CardContent>
-        <CardContent>
-          Our club's mission is to provide experience and opportunities to all
-          engineering students, regardless of their past experiences. Please
-          keep this in mind as you complete your application. <br /> <br />
-          There are 5 sections with a resume upload at the end. Make sure you
-          have your resume downloaded on your current device before proceeding.{" "}
-          <br /> <br />
-          Please note: Applications with missing or incomplete responses will be
-          automatically rejected. We encourage you to take your time and
-          thoroughly fill out the application. TAMU ThinkTank (TTT) reserves the
-          right to vary the evaluation criteria from semester to semester based
-          on the number of applicants, available teams, competition
-          requirements, and other relevant factors.
-          <br /> <br />
-        </CardContent>
-        <CardContent>
-          Our club's mission is to provide experience and opportunities to all
-          engineering students, regardless of their past experiences. Please
-          keep this in mind as you complete your application. <br /> <br />
-          There are 5 sections with a resume upload at the end. Make sure you
-          have your resume downloaded on your current device before proceeding.{" "}
-          <br /> <br />
-          Please note: Applications with missing or incomplete responses will be
-          automatically rejected. We encourage you to take your time and
-          thoroughly fill out the application. TAMU ThinkTank (TTT) reserves the
-          right to vary the evaluation criteria from semester to semester based
-          on the number of applicants, available teams, competition
-          requirements, and other relevant factors.
-          <br /> <br />
-        </CardContent>
       </Card>
       <TabsList className="float-right bg-transparent">
         <TabsTrigger className="bg-white text-black" value="personal">
