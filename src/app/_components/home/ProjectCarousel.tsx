5d4d7360329fb4abc628db67151c8c9926c28190
Krishnan Vellore <vskrishnan@tamu.edu>
2024-01-11 12:19:30 -0600

    lazy loading still sucks

--- src/app/_components/home/ProjectCarousel.tsx
+++ src/app/_components/home/ProjectCarousel.tsx
@@ -70,7 +70,7 @@ export default function ProjectCarousel() {
                   className="w-auto rounded-sm"
                   width={0}
                   height={0}
-                  loading="lazy"
+                  loading="eager"
                 />
               </CardContent>
             </Card>
