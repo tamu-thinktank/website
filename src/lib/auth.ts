6cfc6b1bc6e34862c6fbf1f69755e0b2e6a85a25
anthony pasala <anthonypasala12@gmail.com>
2024-01-18 21:59:48 -0600

    session back in jwt

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -42,6 +42,9 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
     signIn: async ({ user, profile }) => {
       if (user.email && env.ALLOWED_EMAILS.includes(user.email)) {
