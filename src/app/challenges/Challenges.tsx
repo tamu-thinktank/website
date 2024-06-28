b816d65fd93db8b5d9cb2380af502e6fd5977150
Krishnan Vellore <vskrishnan@tamu.edu>
2024-06-28 00:07:44 -0500

    Add BlueSkies to challenges page

--- src/app/challenges/Challenges.tsx
+++ src/app/challenges/Challenges.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Challenges() {
   return (
@@ -15,20 +16,34 @@ export default function Challenges() {
       <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-2">
         <div className="group rounded-3xl border border-gray-100 bg-white bg-opacity-50 p-6 shadow-2xl shadow-gray-600/10 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none sm:p-8">
           <div className="relative overflow-hidden rounded-xl">
-            <Image
-              src="https://d253pvgap36xx8.cloudfront.net/challenges/thumbnail/8d50149c82f611ec8d16862fe09b0a86/8c0628a682f611eca2ab8a8fe6f2b952.webp"
-              alt="art cover"
-              width="1000"
-              height="667"
-              className="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"
-            />
+          <Link href="https://blueskies.nianet.org/competition/" passHref>
+              <Image
+                src="https://blueskies.nianet.org/wp-content/uploads/BlueSkies_Logo_lg-1.png"
+                alt="art cover"
+                width="1"
+                height="1"
+                className="h-full w-full object-cover object-top transition duration-500 group-hover:scale-105"
+              />
+          </Link>
           </div>
           <div className="relative mt-6">
             <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
-              Challenge 1
+              NASA'S Gateways to Blue Skies Competition
             </h3>
             <p className="mb-8 mt-6 text-gray-600 dark:text-gray-300">
-              Challenge Description 1
+            Blue Skies expands engagement between universities and NASA's University 
+            Innovation Project, industry, and government partners by providing an 
+            opportunity for multi-disciplinary teams of students from all academic 
+            levels (i.e., freshman, sophomore, junior, senior, and graduate) to tackle 
+            significant challenges and opportunities for the aviation industry through 
+            a new project theme each year. The competition is guided by a push toward 
+            new technologies as well as environmentally and socially conscious aviation. 
+            Initial participation involves a conceptual study, submission of a 5-7 page 
+            proposal and video summarizing the team’s proposal. Finalists write a final 
+            research paper, create an infographic summarizing concepts, and present in 
+            front of NASA and industry experts at a culminating forum held at a NASA 
+            Center each June. Internship opportunities with NASA’s ARMD serve as the 
+            competition prize for members of the winning team.
             </p>
           </div>
         </div>
