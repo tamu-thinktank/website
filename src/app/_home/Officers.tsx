7670201715dd6067677b169a7162c47ffc49fc59
vskrishnan <vskrishnan@tamu.edu>
2024-08-05 19:45:18 -0500

    update officers

--- src/app/_home/Officers.tsx
+++ src/app/_home/Officers.tsx
@@ -15,24 +15,7 @@ export default function Officers() {
             <div className="flex items-center justify-center gap-4">
               <Image
                 className="h-auto w-32 rounded-full"
-                src="/images/avatars/president.webp"
-                alt="user avatar"
-                width={500}
-                height={500}
-              />
-            </div>
-            <h1 className="mt-4 text-lg font-medium text-gray-700 dark:text-white">
-              Krishnan Vellore
-            </h1>
-            <p className="text-sm text-gray-500 dark:text-gray-300">
-              President
-            </p>
-          </div>
-          <div className="aspect-auto rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none">
-            <div className="flex items-center justify-center gap-4">
-              <Image
-                className="h-auto w-32 rounded-full"
-                src="/images/avatars/vp.webp"
+                src="/images/avatars/garrett.webp"
                 alt="user avatar"
                 width={500}
                 height={500}
@@ -42,6 +25,23 @@ export default function Officers() {
               Garrett Stevenson
             </h1>
             <p className="text-sm text-gray-500 dark:text-gray-300">
+              President
+            </p>
+          </div>
+          <div className="aspect-auto rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none">
+            <div className="flex items-center justify-center gap-4">
+              <Image
+                className="h-auto w-32 rounded-full"
+                src="/images/avatars/payas.webp"
+                alt="user avatar"
+                width={500}
+                height={500}
+              />
+            </div>
+            <h1 className="mt-4 text-lg font-medium text-gray-700 dark:text-white">
+              Payas Joshi
+            </h1>
+            <p className="text-sm text-gray-500 dark:text-gray-300">
               Vice President
             </p>
           </div>
@@ -49,7 +49,7 @@ export default function Officers() {
             <div className="flex items-center justify-center gap-4">
               <Image
                 className="h-auto w-32 rounded-full"
-                src="/images/avatars/ctc.webp"
+                src="/images/avatars/aiden.webp"
                 alt="user avatar"
                 width={500}
                 height={500}
@@ -59,6 +59,23 @@ export default function Officers() {
               Aiden Kampwerth
             </h1>
             <p className="text-sm text-gray-500 dark:text-gray-300">
+              Competition Team Administrator
+            </p>
+          </div>
+          <div className="aspect-auto rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none">
+            <div className="flex items-center justify-center gap-4">
+              <Image
+                className="h-auto w-32 rounded-full"
+                src="/images/avatars/arjun.webp"
+                alt="user avatar"
+                width={500}
+                height={500}
+              />
+            </div>
+            <h1 className="mt-4 text-lg font-medium text-gray-700 dark:text-white">
+              Arjun Sawheny
+            </h1>
+            <p className="text-sm text-gray-500 dark:text-gray-300">
               Competition Team Coordinator
             </p>
           </div>
@@ -66,7 +83,24 @@ export default function Officers() {
             <div className="flex items-center justify-center gap-4">
               <Image
                 className="h-auto w-32 rounded-full"
-                src="/images/avatars/pr.webp"
+                src="/images/avatars/moksh.webp"
+                alt="user avatar"
+                width={500}
+                height={500}
+              />
+            </div>
+            <h1 className="mt-4 text-lg font-medium text-gray-700 dark:text-white">
+              Moksh Shah
+            </h1>
+            <p className="text-sm text-gray-500 dark:text-gray-300">
+              Competition Team Coordinator
+            </p>
+          </div>
+          <div className="aspect-auto rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none">
+            <div className="flex items-center justify-center gap-4">
+              <Image
+                className="h-auto w-32 rounded-full"
+                src="/images/avatars/jaeson.webp"
                 alt="user avatar"
                 width={500}
                 height={500}
@@ -83,7 +117,7 @@ export default function Officers() {
             <div className="flex items-center justify-center gap-4">
               <Image
                 className="h-auto w-32 rounded-full"
-                src="/images/avatars/treasurer.webp"
+                src="/images/avatars/landon.webp"
                 alt="user avatar"
                 width={500}
                 height={500}
@@ -94,6 +128,23 @@ export default function Officers() {
             </h1>
             <p className="text-sm text-gray-500 dark:text-gray-300">
               Treasurer
+            </p>
+          </div>
+          <div className="aspect-auto rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none">
+            <div className="flex items-center justify-center gap-4">
+              <Image
+                className="h-auto w-32 rounded-full"
+                src="/images/avatars/krishnan.webp"
+                alt="user avatar"
+                width={500}
+                height={500}
+              />
+            </div>
+            <h1 className="mt-4 text-lg font-medium text-gray-700 dark:text-white">
+              Krishnan Vellore
+            </h1>
+            <p className="text-sm text-gray-500 dark:text-gray-300">
+              Advisory Officer
             </p>
           </div>
         </div>
