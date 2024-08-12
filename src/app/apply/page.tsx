ff442eb3d27f0e7019956d5724115585e1be552f
vskrishnan <vskrishnan@tamu.edu>
2024-08-11 19:33:01 -0500

    format fix

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -64,7 +64,8 @@ export default function Apply() {
   const { mutateAsync: submitForm } = api.public.apply.useMutation({
     onSuccess: () => {
       toast({
-        title: "✅ Form submitted! \n Contact tamuthinktank@gmail.com if you do not recieve an email by Sept. 9th.",
+        title:
+          "✅ Form submitted! \n Contact tamuthinktank@gmail.com if you do not recieve an email by Sept. 9th.",
         duration: 5000,
       });
 
