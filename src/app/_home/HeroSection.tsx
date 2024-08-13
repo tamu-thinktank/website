507bea8554bd5cd8d06105ea2ba201d81d36da03
vskrishnan <vskrishnan@tamu.edu>
2024-08-12 21:36:06 -0500

    chore: Update Discord link in HeroSection

--- src/app/_home/HeroSection.tsx
+++ src/app/_home/HeroSection.tsx
@@ -53,11 +53,11 @@ export default function HeroSection() {
             </p>
             <div className="mt-16 flex flex-wrap justify-center gap-x-6 gap-y-4">
               <a
-                href="#overview"
+                href="https://discord.gg/qUAuSraYV9"
                 className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:bg-primary-foreground sm:w-max"
               >
                 <span className="relative text-base font-semibold text-white">
-                  Learn more
+                  Discord
                 </span>
               </a>
               <a
