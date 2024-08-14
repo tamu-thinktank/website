00e2f40015be50613cb65bd2f775dd5bb81574d2
vskrishnan <vskrishnan@tamu.edu>
2024-08-13 21:38:55 -0500

    fix formating

--- emails/interview.tsx
+++ emails/interview.tsx
@@ -76,12 +76,11 @@ export const InterviewEmail = ({
         </Section>
         <Text style={paragraph}>Howdy {userFirstname}!</Text>
         <Text style={paragraph}>
-          TAMU ThinkTank would like to interview you for a spot on one 
-          of the design challenge teams. We believe you could have the 
-          skills and talents necessary to compete and excel in one of 
-          our design challenge teams. This interview is the final step 
-          for us to determine if you would be a good fit in the 
-          organization and on a team.
+          TAMU ThinkTank would like to interview you for a spot on one of the
+          design challenge teams. We believe you could have the skills and
+          talents necessary to compete and excel in one of our design challenge
+          teams. This interview is the final step for us to determine if you
+          would be a good fit in the organization and on a team.
         </Text>
         <Text style={paragraph}>
           The details for your interview are as follows:
@@ -91,17 +90,16 @@ export const InterviewEmail = ({
           <strong>Interviewer:</strong> {interviewerName}
         </Text>
         <Text style={paragraph}>
-          These details are optimized to match your availability 
-          with ours, but let us know if these do not work for you 
-          and we will try and accommodate you. Accommodations will 
-          only be made for academic, disability, family, or other 
-          related matters. If you're unsure, contact us and we'll 
-          discuss it.
+          These details are optimized to match your availability with ours, but
+          let us know if these do not work for you and we will try and
+          accommodate you. Accommodations will only be made for academic,
+          disability, family, or other related matters. If you're unsure,
+          contact us and we'll discuss it.
         </Text>
         <Text style={paragraph}>
-          Prior to your interview, we require you to join our discord 
-          server in order to receive information about your application 
-          status in ThinkTank. 
+          Prior to your interview, we require you to join our discord server in
+          order to receive information about your application status in
+          ThinkTank.
         </Text>
         <a
           href="https://discord.gg/9Fjkstq4kf"
@@ -112,41 +110,48 @@ export const InterviewEmail = ({
           </span>
         </a>
         <Text style={paragraph}>
-          Once in the discord, please change your server nickname to your 
-          preferred first name and last name. Additionally, ensure that 
-          notifications for the ThinkTank server are enabled. This is 
-          <strong>VERY IMPORTANT</strong> and failure to do so before 
-          the interview will result in the <strong>IMMEDIATE</strong> 
+          Once in the discord, please change your server nickname to your
+          preferred first name and last name. Additionally, ensure that
+          notifications for the ThinkTank server are enabled. This is
+          <strong>VERY IMPORTANT</strong> and failure to do so before the
+          interview will result in the <strong>IMMEDIATE</strong>
           dismissal of your application.
         </Text>
         <Text style={paragraph}>
-          Additionally, any student applying for an “open-ended” challenge as 
-          their first choice is required to come up with a general solution to 
-          their challenge before the interview. This solution doesn't need to 
-          be thoroughly thought out but should clearly demonstrate understanding 
-          of the rules and conditions of the challenge. Failure to produce a 
-          basic high level conceptual solution will result in a harsh consideration 
+          Additionally, any student applying for an “open-ended” challenge as
+          their first choice is required to come up with a general solution to
+          their challenge before the interview. This solution doesn't need to be
+          thoroughly thought out but should clearly demonstrate understanding of
+          the rules and conditions of the challenge. Failure to produce a basic
+          high level conceptual solution will result in a harsh consideration
           against your application.
         </Text>
         <Text style={paragraph}>
           This season, we are offering the following “open-ended” challenges:
           <br />
-          <a href="https://www.aiaa.org/docs/default-source/uploadedfiles/membership-and-communities/university-students/design-competitions/2024-25-aiaa-undergraduate-team-space-design-competition.pdf" 
-             target="_blank" 
-             rel="noopener noreferrer" 
-             style={{ textDecoration: 'none' }}>
-            <strong>AIAA:</strong> Mars Exploration Surveyors to Enable Human Exploration
+          <a
+            href="https://www.aiaa.org/docs/default-source/uploadedfiles/membership-and-communities/university-students/design-competitions/2024-25-aiaa-undergraduate-team-space-design-competition.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            <strong>AIAA:</strong> Mars Exploration Surveyors to Enable Human
+            Exploration
           </a>
           <br />
-          <a href="https://blueskies.nianet.org/competition/" 
-             target="_blank" 
-             rel="noopener noreferrer" 
-             style={{ textDecoration: 'none' }}>
-            <strong>Blue Skies:</strong> AgAir (Aviation Solutions for Agriculture)
+          <a
+            href="https://blueskies.nianet.org/competition/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            <strong>Blue Skies:</strong> AgAir (Aviation Solutions for
+            Agriculture)
           </a>
         </Text>
         <Text style={paragraph}>
-          Notify us if you have any questions or issues with links. We're very excited to meet you!
+          Notify us if you have any questions or issues with links. We're very
+          excited to meet you!
         </Text>
         <Text style={paragraph}>
           Sincerely,
