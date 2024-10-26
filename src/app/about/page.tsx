9eda4853378a08e0f0e77a774ae4ab1fb0afcc7a
jt171516 <jt171516@gmail.com>
2024-10-25 20:35:04 -0500

    added sections to page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,12 +2,16 @@ import AppFooter from "@/components/AppFooter";
 import AppHeader from "@/components/AppHeader";
 import type { NextPage } from "next";
 import CardStack from "./CardStack";
+import Challenges from "./Challenges";
+import OfficerInterest from "./OfficerInterest";
 
 const Home: NextPage = () => {
   return (
     <>
       <AppHeader />
+        <Challenges />
         <CardStack />
+        <OfficerInterest />
       <AppFooter />
     </>
   );
