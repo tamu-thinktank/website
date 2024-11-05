89b09c752d26e70ada155d7d275fc22b806357c0
jt171516 <jt171516@gmail.com>
2024-11-04 23:02:03 -0600

    Added more info to officer page

--- src/app/about/Officers.tsx
+++ src/app/about/Officers.tsx
@@ -7,17 +7,17 @@ export default function Officers() {
       id: 1,
       name: "Garrett Stevenson",
       role: "President",
-      major: "major",
-      class: "year",
+      major: "Aerospace Engineering",
+      class: "25",
       image: "/images/avatars/garrett.webp",
-      bio: "About me",
+      bio: "Competed in several design challenges. Experienced in systems engineering, mission concept development, and radio frequency communications. Loves spacecraft, hiking, and mashed potatoes.",
     },
     {
       id: 2,
       name: "Payas Joshi",
       role: "Vice President",
-      major: "major",
-      class: "year",
+      major: "Industrial Engineering",
+      class: "27",
       image: "/images/avatars/payas.webp",
       bio: "About me",
     },
@@ -28,32 +28,32 @@ export default function Officers() {
       major: "Aerospace Engineering",
       class: "26",
       image: "/images/avatars/aiden.webp",
-      bio: "About me",
+      bio: "Team Lead for Team Vestigo - Won 1st place in competition. Specialty in scientific computing for aerospace applications. Enjoys movies, tennis, and cooking.",
     },
     {
       id: 4,
       name: "Arjun Sawheny",
-      role: "Competition Team Coordinator",
-      major: "major",
-      class: "year",
+      role: "Peer Mentor",
+      major: "Aerospace Engineering",
+      class: "27",
       image: "/images/avatars/arjun.webp",
       bio: "About me",
     },
     {
       id: 5,
       name: "Moksh Shah",
-      role: "Competition Team Coordinator",
-      major: "major",
-      class: "year",
+      role: "Peer Mentor",
+      major: "Aerospace Engineering",
+      class: "27",
       image: "/images/avatars/moksh.webp",
-      bio: "About me",
+      bio: "Part of Team Selenis - Won 1st place at Engineering Project Showcase. Extensive research expertise in Lunar Operations. Loves to play tennis in free time.",
     },
     {
       id: 6,
       name: "Jae-son Rivera",
-      role: "Public Outreach Officer",
-      major: "major",
-      class: "year",
+      role: "Public Relations",
+      major: "Aerospace Engineering",
+      class: "25",
       image: "/images/avatars/jaeson.webp",
       bio: "About me",
     },
@@ -61,19 +61,28 @@ export default function Officers() {
       id: 7,
       name: "Landon Uelsmann",
       role: "Treasurer",
-      major: "major",
-      class: "year",
+      major: "Computer Science",
+      class: "26",
       image: "/images/avatars/landon.webp",
-      bio: "About me",
+      bio: "Part of Team Vestigo - Won 1st place in competition. Interest in cryptography and machine learning. I love to play Fortnite competitively in my free time.",
     },
     {
       id: 8,
+      name: "Ipshita Singh",
+      role: "Marketing Specialist",
+      major: "Mechanical Engineering",
+      class: "27",
+      image: "/images/avatars/ipshita.webp",
+      bio: "Part of Team Astraeus. Worked mainly on the GNC subsystem. Enjoys trying new places to eat and watching good movies.",
+    },
+    {
+      id: 9,
       name: "Krishnan Vellore",
-      role: "Advisory Officer",
-      major: "major",
-      class: "year",
+      role: "Web Dev",
+      major: "Aerospace Engineering",
+      class: "25",
       image: "/images/avatars/krishnan.webp",
-      bio: "About me",
+      bio: "Web Dev",
     },
   ];
 
@@ -105,11 +114,11 @@ export default function Officers() {
   
               {/* Hover Overlay */}
               <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-0 group-hover:bg-opacity-60 transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100">
-                <h3 className="text-xl font-bold text-white">{officer.name}</h3>
-                <p className="text-md font-semibold text-gray-200 mt-1">
+                <h3 className="text-2xl font-bold text-white">{officer.name}</h3>
+                <p className="text-xl font-semibold text-gray-200 mt-1">
                   {officer.major} '{officer.class}
                 </p>
-                <p className="text-sm text-gray-200 mt-3">{officer.role}</p>
+                <p className="text-lg text-gray-200 mt-1">{officer.role}</p>
                 <p className="mt-3 text-sm text-gray-200 px-4 text-center">
                   {officer.bio}
                 </p>
