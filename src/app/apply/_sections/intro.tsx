28a82a96796ad381769658c909404f30ae364dfc
vskrishnan <vskrishnan@tamu.edu>
2024-11-06 12:57:46 -0600

    Unlock Form

--- src/app/apply/_sections/intro.tsx
+++ src/app/apply/_sections/intro.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { TabsContent } from "@/components/ui/tabs";
+import { TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function FormIntroTab() {
   return (
@@ -11,7 +12,7 @@ export default function FormIntroTab() {
           <Separator />
         </CardHeader>
         <CardContent className="text-center">
-          {/* Our club's mission is to provide experience and opportunities to all
+          Our club's mission is to provide experience and opportunities to all
           engineering students, regardless of their past experiences. Please
           keep this in mind as you complete your application. <br /> <br />
           There are 5 sections with a resume upload at the end. Make sure you
@@ -23,15 +24,15 @@ export default function FormIntroTab() {
           right to vary the evaluation criteria from semester to semester based
           on the number of applicants, available teams, competition
           requirements, and other relevant factors.
-          <br /> <br /> */}
+          <br /> <br />
           ❗❗❗❗❗❗ APPLICATIONS ARE NOW CLOSED❗❗❗❗❗❗
         </CardContent>
       </Card>
-      {/* <TabsList className="float-right bg-transparent">
+      <TabsList className="float-right bg-transparent">
         <TabsTrigger className="bg-white text-black" value="personal">
           Next
         </TabsTrigger>
-      </TabsList> */}
+      </TabsList>
     </TabsContent>
   );
 }
