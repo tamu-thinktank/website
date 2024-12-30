01b3427d26560370819a15402274670076a208e2
luvadlamudi <lucasvad123@gmail.com>
2024-12-30 16:50:59 -0600

    format fix attempt 2

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,22 @@ const config = {
   plugins: ["prettier-plugin-tailwindcss"],
   tabWidth: 2,
   useTabs: false,
+  singleQuote: false, // Ensures double quotes in JSON and JavaScript
+  overrides: [
+    {
+      files: "*.json",
+      options: {
+        parser: "json",
+        singleQuote: false, // JSON requires double quotes
+      },
+    },
+    {
+      files: "*.yaml",
+      options: {
+        parser: "yaml",
+      },
+    },
+  ],
 };
 
 export default config;
