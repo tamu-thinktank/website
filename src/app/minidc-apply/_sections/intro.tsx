3dea1dad5d2bf50052eac3a2ab4a93c1f690944f
luvadlamudi <lucasvad123@gmail.com>
2025-03-31 12:10:36 -0500

    locked apps

--- src/app/minidc-apply/_sections/intro.tsx
+++ src/app/minidc-apply/_sections/intro.tsx
@@ -12,29 +12,33 @@ export default function FormIntroTab() {
           <Separator />
         </CardHeader>
         <CardContent className="text-center">
-          Our club’s mission is to provide experience and opportunities to all 
-          engineering students, regardless of their prior experiences. 
-          Please keep this in mind as you complete your application.
+          Our club’s mission is to provide experience and opportunities to all
+          engineering students, regardless of their prior experiences. Please
+          keep this in mind as you complete your application.
           <br /> <br />
-          There are 2 multiple-choice sections and 1 open-end question section with a resume upload at the end. 
-          We recommend writing your free responses in a separate document and transferring them to the application. 
-          Additionally, we recommend having your resume downloaded on your device before continuing.{" "}
-          <br /> <br />
-          Please note: Applications with missing or incomplete responses will be automatically rejected. 
-          We encourage you to take your time and thoroughly fill out the application. 
-          ThinkTank reserves the right to vary evaluation criteria each recruitment cycle based on number of applications and other relevant factors.
+          There are 2 multiple-choice sections and 1 open-end question section
+          with a resume upload at the end. We recommend writing your free
+          responses in a separate document and transferring them to the
+          application. Additionally, we recommend having your resume downloaded
+          on your device before continuing. <br /> <br />
+          Please note: Applications with missing or incomplete responses will be
+          automatically rejected. We encourage you to take your time and
+          thoroughly fill out the application. ThinkTank reserves the right to
+          vary evaluation criteria each recruitment cycle based on number of
+          applications and other relevant factors.
           <br /> <br />
           <strong>
-          WE HIGHLY RECOMMEND READING THE MINI DC FAQ AND COMPLETING THE APPLICATION ON A LAPTOP/DESKTOP
+            WE HIGHLY RECOMMEND READING THE MINI DC FAQ AND COMPLETING THE
+            APPLICATION ON A LAPTOP/DESKTOP
           </strong>
           {/* <br /> <br />
           <strong>❗❗❗❗❗❗ APPLICATIONS ARE NOW CLOSED❗❗❗❗❗❗</strong> */}
         </CardContent>
       </Card>
       <TabsList className="float-right bg-transparent">
-        <TabsTrigger className="bg-white text-black" value="personal">
-          Next
-        </TabsTrigger>
+        {/* <TabsTrigger className="bg-white text-black" value="personal">
+          Closed
+        </TabsTrigger> */}
       </TabsList>
     </TabsContent>
   );
