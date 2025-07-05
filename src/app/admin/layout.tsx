43deced55c2d23dfdb0d1cc6270e581d289cea82
vskrishnan <vskrishnan@tamu.edu>
2025-07-05 16:47:49 -0500

    #58 - Fix app\admin\layout.txt for lint

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import GradientLayout from "@/components/GradientLayout";
 import { Button } from "@/components/ui/button";
 import { getBaseUrl } from "@/lib/trpc/shared";
 import { Loader2 } from "lucide-react";
 import { SessionProvider, signIn, useSession } from "next-auth/react";
-import { AdminHeader } from "./admin-header";
 import Nav from "../../components/AdminTopFooter";
 
 export default function AdminLayout({
