9cb2fd461dbcc6e9b3c454a4aa5b3ed0058ce5cf
tamuthinktank <156534423+tamuthinktank@users.noreply.github.com>
2025-09-03 16:47:25 -0500

    challenge text update 1

--- src/app/challenges/Challenges.tsx
+++ src/app/challenges/Challenges.tsx
@@ -7,37 +7,6 @@ import Container from "@/components/Container";
 const Challenges = () => {
   const teams = [
     {
-      teamName: "Daedalus",
-      shortOverview: "Hands-On NASA Research Project",
-      competitionOverview:
-        "Texas Space Grant Consortium (TSGC) offers unique, hands-on challenges sponsored by NASA mentors which rotate each year. We can’t guarantee what you will be working on, but we can guarantee it will be an amazing experience!",
-      duration: "1 Semester",
-      teamSize: "6",
-      admissionTimeline: [
-        { date: "Applications", description: "11/21/25" },
-        { date: "Interviews", description: "1/17/26" },
-        { date: "Acceptance", description: "1/26/26" },
-      ],
-      pastTeams: [
-        "Astronaut Tracking System",
-        "Microgravity Water Filtration",
-        "Lunar Cellular Infrastructure",
-      ],
-      mentors: {
-        peer: "Anoushka Kaushik",
-        faculty: "TBD",
-      },
-      competitionLink: "https://ig.utexas.edu/tsgc/design-challenge/",
-      imageUrl: "/images/Daedalus.png",
-      image: "/images/avatars/anoushkakaushik2526",
-      major: "Aerospace",
-      year: "28",
-      description: "About Me",
-      facultyImage: "blank.webp",
-      prize: "$6,000 for 1st place",
-      dtype: "Showcase",
-    },
-    {
       teamName: "Atlas",
       shortOverview: "Design ThinkTank’s Next-Gen Website",
       competitionOverview:
@@ -282,6 +251,37 @@ const Challenges = () => {
         "Grand Prize of $10,000 and three additional awards granted at $5,000, $3,000 and $1,000 each",
       dtype: "Proposal",
     },
+    {
+      teamName: "Daedalus",
+      shortOverview: "Hands-On NASA Research Project",
+      competitionOverview:
+        "Texas Space Grant Consortium (TSGC) offers unique, hands-on challenges sponsored by NASA mentors which rotate each year. We can’t guarantee what you will be working on, but we can guarantee it will be an amazing experience!",
+      duration: "1 Semester",
+      teamSize: "6",
+      admissionTimeline: [
+        { date: "Applications", description: "11/21/25" },
+        { date: "Interviews", description: "1/17/26" },
+        { date: "Acceptance", description: "1/26/26" },
+      ],
+      pastTeams: [
+        "Astronaut Tracking System",
+        "Microgravity Water Filtration",
+        "Lunar Cellular Infrastructure",
+      ],
+      mentors: {
+        peer: "Anoushka Kaushik",
+        faculty: "TBD",
+      },
+      competitionLink: "https://ig.utexas.edu/tsgc/design-challenge/",
+      imageUrl: "/images/Daedalus.png",
+      image: "/images/avatars/anoushkakaushik2526",
+      major: "Aerospace",
+      year: "28",
+      description: "About Me",
+      facultyImage: "blank.webp",
+      prize: "$6,000 for 1st place",
+      dtype: "Showcase",
+    },
   ];
 
   return (
@@ -316,3 +316,4 @@ const Challenges = () => {
 };
 
 export default Challenges;
+
