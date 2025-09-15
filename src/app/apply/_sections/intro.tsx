e2b11f17419f5c8aa51a6fe4e30b070b6b057473
tamuthinktank <tamuthinktank@gmail.com>
2025-09-15 00:49:37 -0500

    more lint fixes

--- src/app/apply/_sections/intro.tsx
+++ src/app/apply/_sections/intro.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { TabsContent, TabsTrigger } from "@/components/ui/tabs";
-import { TabsList } from "@/components/ui/tabs";
+import { TabsContent } from "@/components/ui/tabs";
+//import { TabsList } from "@/components/ui/tabs";
 
 export default function FormIntroTab() {
   return (
